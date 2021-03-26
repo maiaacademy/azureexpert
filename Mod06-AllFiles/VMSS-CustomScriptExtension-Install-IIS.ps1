137d7c401d5dfae1b6c3c3ab6bb852b70589f217
maiaacademy <76057144+maiaacademy@users.noreply.github.com>
2021-03-26 16:03:13 -0300

    Insert

---
+++ Mod06-AllFiles/VMSS-CustomScriptExtension-Install-IIS.ps1
@@ -0,0 +1,21 @@
+﻿# Connect Azure
+Connect-AzAccount
+
+# Set Script 
+$customConfig = @{ 
+  "fileUris" = (,"SourceGitHub"); 
+  "commandToExecute" = "powershell -ExecutionPolicy Unrestricted -File ScriptName.ps1" 
+} 
+
+# Set VMSS variables
+$rgname = "ResourceGroup"
+$vmssname = "VMSSName"
+ 
+# Get VMSS object 
+$vmss = Get-AzVmss -ResourceGroupName $rgname -VMScaleSetName $vmssname
+ 
+# Add VMSS extension 
+$vmss = Add-AzVmssExtension -Name "CustomScript" -VirtualMachineScaleSet $vmss -Publisher "Microsoft.Compute" -Type "CustomScriptExtension" -TypeHandlerVersion "1.9" -Setting $customConfig 
+ 
+# Update VMMS 
+Update-AzVmss -ResourceGroupName $rgname -Name $vmssname -VirtualMachineScaleSet $vmss 
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Treinamento Azure Expert
+
+The code in this repository contains all code for the Azure Expert.
