f18004eeae1eed1d11f98258e1c9f07e74795b95
Maia Academy - Guilherme Maia <76057144+maiaacademy@users.noreply.github.com>
2021-03-27 17:01:34 -0300

    Delete VMSS-Install-IIS_v2.ps1

--- Mod06-AllFiles/VMSS-Install-IIS_v2.ps1
+++
@@ -1,5 +0,0 @@
-﻿# Install Web-Server
-Add-WindowsFeature Web-Server
-
-# Set Default HTM
-Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Azure Expert v2 is running $($env:computername)"
