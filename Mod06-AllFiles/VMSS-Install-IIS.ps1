c35abd8d0c1271222ebf41d5c41ea533ca97489f
maiaacademy <76057144+maiaacademy@users.noreply.github.com>
2021-03-27 13:11:02 -0300

    Update VMSS-Install-IIS.ps1

--- Mod06-AllFiles/VMSS-Install-IIS.ps1
+++ Mod06-AllFiles/VMSS-Install-IIS.ps1
@@ -2,4 +2,4 @@
 Add-WindowsFeature Web-Server
 
 # Set Default HTM
-Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Azure Expert v2 is running $($env:computername)"
+Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Azure Expert is running $($env:computername)"
